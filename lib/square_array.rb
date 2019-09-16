4a145f602631a787d770d403ec72f3103ba64b3d
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 17:42:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do
-   new_array.push() array[counter] ** 2 
+   new_array.push(array[counter] ** 2)  
     counter += 1 
-    p array
+    p new_array
 end
